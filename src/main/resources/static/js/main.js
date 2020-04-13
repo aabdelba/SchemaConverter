e1c45bbd9c25bb6d6cd580d49692ecf21a0274cd
Ali Abdelbadie <aliabdelbadie@gmail.com>
2020-04-13 15:23:38 +0300

    cancel button on loading screen works more specific error handling page controller now reads in form values

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,40 +1,20 @@
 $(function(){
 
     $('#form1').submit(function(){
-    $('#formLoadingToggle').html(
-
-'    <div class="row empty-100-row"></div>                                      '+
-
-
-
-'    <p class="text-center">Running the conversion...</p>                       '+
-
-'    <div class="row empty-10-row"></div>                                       '+
-
-'    <div class="center-button">                                                '+
-'        <div class="spinner-border text-primary" role="status">                '+
-'            <span class="sr-only">Loading...</span>                            '+
-'        </div>                                                                 '+
-'    </div>                                                                     '+
-
-'    <div class="row empty-25-row"></div>                                       '+
-
-'    <div class="center-button">                                                '+
-
-'        <form action="/form" method="post" id="cancelForm">                    '+
-'            <button class="btn btn-danger" type="submit" form="cancelForm"     '+
-'                                           value="Submit">Cancel</button>      '+
-'        </form>                                                                '+
-
-'    </div>                                                                     '+
-
-'    <div class="row empty-100-row"></div>                                      '
-
-
-
-
-
-
-        );
+        $('#formToggle').toggleClass("hidden");
+        $('#loadingToggle').toggleClass("hidden");
     });
+
 });
+
+function goBack() {
+    window.history.back();
+}
+
+
+function refresh() {
+//        $("#loadingToggle").toggleClass("hidden");
+//        $('#formToggle').toggleClass("hidden");
+        location.reload();
+//        window.history.back();
+}
